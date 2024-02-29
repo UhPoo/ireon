467342971cc7794e8900ce88b9e35cdba115e887
longBright <longbright0804@gmail.com>
2024-02-29 22:20:28 +0900

    IREON-51/[유기동물 등록] test: 테스트용 공통 클래스 생성

---
+++ ireon/src/test/java/com/uhpoo/ireon/IntegrationTestSupport.java
@@ -0,0 +1,14 @@
+package com.uhpoo.ireon;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 테스트용 공통 클래스
+ *
+ * @author 최영환
+ */
+@SpringBootTest
+@Transactional
+public abstract class IntegrationTestSupport {
+}
