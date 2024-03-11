c53203a90baabfefe1ae7e2c0a455d86da8ecdae
longBright <longbright0804@gmail.com>
2024-03-11 20:48:28 +0900

    IREON-70/[유기동물 전체조회] refactor: AbandonService @Transactional 추가

--- ireon/src/main/java/com/uhpoo/ireon/api/service/abandon/AbandonService.java
+++ ireon/src/main/java/com/uhpoo/ireon/api/service/abandon/AbandonService.java
@@ -5,16 +5,18 @@ import com.uhpoo.ireon.domain.abandon.repository.AbandonRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
  * 유기동물 게시글 서비스
- * 
+ *
  * @author 최영환
  */
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class AbandonService {
 
     private final AbandonRepository abandonRepository;
@@ -24,7 +26,7 @@ public class AbandonService {
      *
      * @param dto      유기동물 게시글 정보
      * @param nickname 현재 로그인 중인 회원 닉네임
-     * @param file
+     * @param file     등록할 첨부파일
      * @return 등록된 PK 값
      */
     public Long createAbandon(CreateAbandonDto dto, String nickname, MultipartFile file) {
