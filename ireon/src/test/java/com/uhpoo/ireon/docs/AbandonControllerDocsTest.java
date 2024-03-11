ea2177dc2cccce51ee221ac7cf6b32067f7d7c21
longBright <longbright0804@gmail.com>
2024-03-11 20:59:29 +0900

    IREON-70/[유기동물 전체조회] refactor: 전체 조회 API 수정

--- ireon/src/test/java/com/uhpoo/ireon/docs/AbandonControllerDocsTest.java
+++ ireon/src/test/java/com/uhpoo/ireon/docs/AbandonControllerDocsTest.java
@@ -149,17 +149,12 @@ public class AbandonControllerDocsTest extends RestDocsSupport {
                 .title("제목1")
                 .author("작성자1")
                 .animalType("개")
-                .animalDetail("말티즈")
-                .animalGender("암컷")
-                .animalAge(5)
-                .vaccinationStatus(VaccinationStatus.THIRD.getText())
-                .neutralized(true)
                 .abandonStatus(AbandonStatus.SEARCHING.getText())
-                .zipcode("11111")
                 .roadAddress("서울시 송파구 토성로")
                 .jibunAddress("서울시 송파구 풍납동")
                 .detailAddress("비밀")
                 .phoneNumber("010-1234-5678")
+                .clipped(true)
                 .createdDate("2024-03-05")
                 .build();
 
@@ -167,17 +162,12 @@ public class AbandonControllerDocsTest extends RestDocsSupport {
                 .title("제목2")
                 .author("작성자2")
                 .animalType("고양이")
-                .animalDetail("코리안 숏헤어")
-                .animalGender("수컷")
-                .animalAge(1)
-                .vaccinationStatus(VaccinationStatus.THIRD.getText())
-                .neutralized(false)
                 .abandonStatus(AbandonStatus.SEARCHING.getText())
-                .zipcode("11111")
                 .roadAddress("서울시 송파구 토성로")
                 .jibunAddress("서울시 송파구 풍납동")
                 .detailAddress("비밀")
                 .phoneNumber("010-1234-5678")
+                .clipped(false)
                 .createdDate("2024-03-04")
                 .build();
 
@@ -216,20 +206,8 @@ public class AbandonControllerDocsTest extends RestDocsSupport {
                                         .description("작성자"),
                                 fieldWithPath("data.items[].animalType").type(JsonFieldType.STRING)
                                         .description("동물 종류"),
-                                fieldWithPath("data.items[].animalDetail").type(JsonFieldType.STRING)
-                                        .description("동물 종류 상세"),
-                                fieldWithPath("data.items[].animalGender").type(JsonFieldType.STRING)
-                                        .description("동물 성별"),
-                                fieldWithPath("data.items[].animalAge").type(JsonFieldType.NUMBER)
-                                        .description("동물 나이"),
-                                fieldWithPath("data.items[].vaccinationStatus").type(JsonFieldType.STRING)
-                                        .description("접종 상태"),
-                                fieldWithPath("data.items[].neutralized").type(JsonFieldType.BOOLEAN)
-                                        .description("중성화 여부"),
                                 fieldWithPath("data.items[].abandonStatus").type(JsonFieldType.STRING)
                                         .description("유기동물 상태"),
-                                fieldWithPath("data.items[].zipcode").type(JsonFieldType.STRING)
-                                        .description("우편번호"),
                                 fieldWithPath("data.items[].roadAddress").type(JsonFieldType.STRING)
                                         .description("도로명 주소"),
                                 fieldWithPath("data.items[].jibunAddress").type(JsonFieldType.STRING)
@@ -238,6 +216,8 @@ public class AbandonControllerDocsTest extends RestDocsSupport {
                                         .description("상세 주소"),
                                 fieldWithPath("data.items[].phoneNumber").type(JsonFieldType.STRING)
                                         .description("연락처"),
+                                fieldWithPath("data.items[].clipped").type(JsonFieldType.BOOLEAN)
+                                        .description("스크랩 여부"),
                                 fieldWithPath("data.items[].createdDate").type(JsonFieldType.STRING)
                                         .description("작성일")
                         )
