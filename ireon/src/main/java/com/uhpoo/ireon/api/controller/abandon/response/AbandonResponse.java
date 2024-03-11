6af91cf68eaea32922a7fdd1661e3d2c88ae84b8
longBright <longbright0804@gmail.com>
2024-03-11 20:59:29 +0900

    IREON-70/[유기동물 전체조회] refactor: 전체 조회 API 수정

--- ireon/src/main/java/com/uhpoo/ireon/api/controller/abandon/response/AbandonResponse.java
+++ ireon/src/main/java/com/uhpoo/ireon/api/controller/abandon/response/AbandonResponse.java
@@ -8,41 +8,32 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AbandonResponse {
 
+    private Long abandonId;
     private String title;
     private String author;
     private String animalType;
-    private String animalDetail;
-    private String animalGender;
-    private Integer animalAge;
-    private String vaccinationStatus;
-    private Boolean neutralized;
     private String abandonStatus;
-    private String zipcode;
     private String roadAddress;
     private String jibunAddress;
     private String detailAddress;
     private String phoneNumber;
+    private Boolean clipped;
     private String createdDate;
 
     @Builder
-    public AbandonResponse(String title, String author, String animalType, String animalDetail, String animalGender,
-                           Integer animalAge, String vaccinationStatus, Boolean neutralized, String abandonStatus,
-                           String zipcode, String roadAddress, String jibunAddress, String detailAddress,
-                           String phoneNumber, String createdDate) {
+    public AbandonResponse(Long abandonId, String title, String author, String animalType, String abandonStatus,
+                           String roadAddress, String jibunAddress, String detailAddress, String phoneNumber,
+                           Boolean clipped, String createdDate) {
+        this.abandonId = abandonId;
         this.title = title;
         this.author = author;
         this.animalType = animalType;
-        this.animalDetail = animalDetail;
-        this.animalGender = animalGender;
-        this.animalAge = animalAge;
-        this.vaccinationStatus = vaccinationStatus;
-        this.neutralized = neutralized;
         this.abandonStatus = abandonStatus;
-        this.zipcode = zipcode;
         this.roadAddress = roadAddress;
         this.jibunAddress = jibunAddress;
         this.detailAddress = detailAddress;
         this.phoneNumber = phoneNumber;
+        this.clipped = clipped;
         this.createdDate = createdDate;
     }
 }
--- ireon/src/test/java/com/uhpoo/ireon/docs/AbandonControllerDocsTest.java
+++ ireon/src/test/java/com/uhpoo/ireon/docs/AbandonControllerDocsTest.java
@@ -146,38 +146,30 @@ public class AbandonControllerDocsTest extends RestDocsSupport {
     void getAbandons() throws Exception {
 
         AbandonResponse item1 = AbandonResponse.builder()
+                .abandonId(3L)
                 .title("제목1")
                 .author("작성자1")
                 .animalType("개")
-                .animalDetail("말티즈")
-                .animalGender("암컷")
-                .animalAge(5)
-                .vaccinationStatus(VaccinationStatus.THIRD.getText())
-                .neutralized(true)
                 .abandonStatus(AbandonStatus.SEARCHING.getText())
-                .zipcode("11111")
                 .roadAddress("서울시 송파구 토성로")
                 .jibunAddress("서울시 송파구 풍납동")
                 .detailAddress("비밀")
                 .phoneNumber("010-1234-5678")
+                .clipped(true)
                 .createdDate("2024-03-05")
                 .build();
 
         AbandonResponse item2 = AbandonResponse.builder()
+                .abandonId(1L)
                 .title("제목2")
                 .author("작성자2")
                 .animalType("고양이")
-                .animalDetail("코리안 숏헤어")
-                .animalGender("수컷")
-                .animalAge(1)
-                .vaccinationStatus(VaccinationStatus.THIRD.getText())
-                .neutralized(false)
                 .abandonStatus(AbandonStatus.SEARCHING.getText())
-                .zipcode("11111")
                 .roadAddress("서울시 송파구 토성로")
                 .jibunAddress("서울시 송파구 풍납동")
                 .detailAddress("비밀")
                 .phoneNumber("010-1234-5678")
+                .clipped(false)
                 .createdDate("2024-03-04")
                 .build();
 
@@ -210,26 +202,16 @@ public class AbandonControllerDocsTest extends RestDocsSupport {
                                         .description("다음 페이지 존재 여부"),
                                 fieldWithPath("data.items").type(JsonFieldType.ARRAY)
                                         .description("게시글 목록"),
+                                fieldWithPath("data.items[].abandonId").type(JsonFieldType.NUMBER)
+                                        .description("유기동물 게시글 PK"),
                                 fieldWithPath("data.items[].title").type(JsonFieldType.STRING)
                                         .description("글 제목"),
                                 fieldWithPath("data.items[].author").type(JsonFieldType.STRING)
                                         .description("작성자"),
                                 fieldWithPath("data.items[].animalType").type(JsonFieldType.STRING)
                                         .description("동물 종류"),
-                                fieldWithPath("data.items[].animalDetail").type(JsonFieldType.STRING)
-                                        .description("동물 종류 상세"),
-                                fieldWithPath("data.items[].animalGender").type(JsonFieldType.STRING)
-                                        .description("동물 성별"),
-                                fieldWithPath("data.items[].animalAge").type(JsonFieldType.NUMBER)
-                                        .description("동물 나이"),
-                                fieldWithPath("data.items[].vaccinationStatus").type(JsonFieldType.STRING)
-                                        .description("접종 상태"),
-                                fieldWithPath("data.items[].neutralized").type(JsonFieldType.BOOLEAN)
-                                        .description("중성화 여부"),
                                 fieldWithPath("data.items[].abandonStatus").type(JsonFieldType.STRING)
                                         .description("유기동물 상태"),
-                                fieldWithPath("data.items[].zipcode").type(JsonFieldType.STRING)
-                                        .description("우편번호"),
                                 fieldWithPath("data.items[].roadAddress").type(JsonFieldType.STRING)
                                         .description("도로명 주소"),
                                 fieldWithPath("data.items[].jibunAddress").type(JsonFieldType.STRING)
@@ -238,6 +220,8 @@ public class AbandonControllerDocsTest extends RestDocsSupport {
                                         .description("상세 주소"),
                                 fieldWithPath("data.items[].phoneNumber").type(JsonFieldType.STRING)
                                         .description("연락처"),
+                                fieldWithPath("data.items[].clipped").type(JsonFieldType.BOOLEAN)
+                                        .description("스크랩 여부"),
                                 fieldWithPath("data.items[].createdDate").type(JsonFieldType.STRING)
                                         .description("작성일")
                         )
