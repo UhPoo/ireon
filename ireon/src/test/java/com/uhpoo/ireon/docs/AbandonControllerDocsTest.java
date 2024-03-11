c900a0e860823b754984361f2d527e5c19877507
longBright <longbright0804@gmail.com>
2024-03-11 20:47:55 +0900

    IREON-56/[유기동물 전체조회] docs: 유기동물 전체 조회 API Docs 작성

--- ireon/src/test/java/com/uhpoo/ireon/docs/AbandonControllerDocsTest.java
+++ ireon/src/test/java/com/uhpoo/ireon/docs/AbandonControllerDocsTest.java
@@ -110,7 +110,7 @@ public class AbandonControllerDocsTest extends RestDocsSupport {
                                 fieldWithPath("animalAge").type(JsonFieldType.NUMBER)
                                         .description("동물 나이"),
                                 fieldWithPath("vaccinationStatus").type(JsonFieldType.STRING)
-                                        .description("접종 여부"),
+                                        .description("접종 상태"),
                                 fieldWithPath("neutralized").type(JsonFieldType.BOOLEAN)
                                         .description("중성화 여부"),
                                 fieldWithPath("abandonStatus").type(JsonFieldType.STRING)
@@ -147,19 +147,37 @@ public class AbandonControllerDocsTest extends RestDocsSupport {
 
         AbandonResponse item1 = AbandonResponse.builder()
                 .title("제목1")
-                .animalType("개")
                 .author("작성자1")
-                .foundTime("18:00")
-                .foundLoc("서울시 강동구")
+                .animalType("개")
+                .animalDetail("말티즈")
+                .animalGender("암컷")
+                .animalAge(5)
+                .vaccinationStatus(VaccinationStatus.THIRD.getText())
+                .neutralized(true)
+                .abandonStatus(AbandonStatus.SEARCHING.getText())
+                .zipcode("11111")
+                .roadAddress("서울시 송파구 토성로")
+                .jibunAddress("서울시 송파구 풍납동")
+                .detailAddress("비밀")
+                .phoneNumber("010-1234-5678")
                 .createdDate("2024-03-05")
                 .build();
 
         AbandonResponse item2 = AbandonResponse.builder()
                 .title("제목2")
-                .animalType("고양이")
                 .author("작성자2")
-                .foundTime("14:00")
-                .foundLoc("서울시 송파구")
+                .animalType("고양이")
+                .animalDetail("코리안 숏헤어")
+                .animalGender("수컷")
+                .animalAge(1)
+                .vaccinationStatus(VaccinationStatus.THIRD.getText())
+                .neutralized(false)
+                .abandonStatus(AbandonStatus.SEARCHING.getText())
+                .zipcode("11111")
+                .roadAddress("서울시 송파구 토성로")
+                .jibunAddress("서울시 송파구 풍납동")
+                .detailAddress("비밀")
+                .phoneNumber("010-1234-5678")
                 .createdDate("2024-03-04")
                 .build();
 
@@ -194,14 +212,32 @@ public class AbandonControllerDocsTest extends RestDocsSupport {
                                         .description("게시글 목록"),
                                 fieldWithPath("data.items[].title").type(JsonFieldType.STRING)
                                         .description("글 제목"),
-                                fieldWithPath("data.items[].animalType").type(JsonFieldType.STRING)
-                                        .description("동물 종류"),
                                 fieldWithPath("data.items[].author").type(JsonFieldType.STRING)
                                         .description("작성자"),
-                                fieldWithPath("data.items[].foundTime").type(JsonFieldType.STRING)
-                                        .description("발견 시간"),
-                                fieldWithPath("data.items[].foundLoc").type(JsonFieldType.STRING)
-                                        .description("발견 장소"),
+                                fieldWithPath("data.items[].animalType").type(JsonFieldType.STRING)
+                                        .description("동물 종류"),
+                                fieldWithPath("data.items[].animalDetail").type(JsonFieldType.STRING)
+                                        .description("동물 종류 상세"),
+                                fieldWithPath("data.items[].animalGender").type(JsonFieldType.STRING)
+                                        .description("동물 성별"),
+                                fieldWithPath("data.items[].animalAge").type(JsonFieldType.NUMBER)
+                                        .description("동물 나이"),
+                                fieldWithPath("data.items[].vaccinationStatus").type(JsonFieldType.STRING)
+                                        .description("접종 상태"),
+                                fieldWithPath("data.items[].neutralized").type(JsonFieldType.BOOLEAN)
+                                        .description("중성화 여부"),
+                                fieldWithPath("data.items[].abandonStatus").type(JsonFieldType.STRING)
+                                        .description("유기동물 상태"),
+                                fieldWithPath("data.items[].zipcode").type(JsonFieldType.STRING)
+                                        .description("우편번호"),
+                                fieldWithPath("data.items[].roadAddress").type(JsonFieldType.STRING)
+                                        .description("도로명 주소"),
+                                fieldWithPath("data.items[].jibunAddress").type(JsonFieldType.STRING)
+                                        .description("지번 주소"),
+                                fieldWithPath("data.items[].detailAddress").type(JsonFieldType.STRING)
+                                        .description("상세 주소"),
+                                fieldWithPath("data.items[].phoneNumber").type(JsonFieldType.STRING)
+                                        .description("연락처"),
                                 fieldWithPath("data.items[].createdDate").type(JsonFieldType.STRING)
                                         .description("작성일")
                         )
