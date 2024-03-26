2b345c02887d0139507f4e25c41ef3ea214581d1
yekk1 <daeyoi96@gmail.com>
2024-03-27 00:09:56 +0900

    IREON-33/실종동물-등록 feat: 실종동물 등록 API 스켈레톤 작성

--- ireon/src/main/java/com/uhpoo/ireon/api/controller/lost/LostController.java
+++ ireon/src/main/java/com/uhpoo/ireon/api/controller/lost/LostController.java
@@ -1,4 +1,48 @@
 package com.uhpoo.ireon.api.controller.lost;
 
+import com.uhpoo.ireon.api.ApiResponse;
+import com.uhpoo.ireon.api.controller.lost.request.CreateLostRequest;
+import com.uhpoo.ireon.api.service.lost.LostService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 실종동물 게시판 API 컨트롤러
+ *
+ * @author yekk1
+ */
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/lost")
 public class LostController {
+
+    private final LostService lostService;
+    /**
+     * 실종동물 게시글 등록 API
+     *
+     * @param request   실종동물 게시글 정보
+     * @param file      첨부파일
+     * @return 등록된 PK 값
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponse<Long> createLost(@Valid @RequestPart(name = "request") CreateLostRequest request, @RequestPart(required = false, name = "file") MultipartFile file) {
+        log.debug("LostController#createLost called.");
+        log.debug("CreateLostRequest={}", request);
+        log.debug("MultipartFile={}", file);
+
+        // 임시 닉네임
+        String nickname = "nickname";
+        log.debug("nickname={}", nickname);
+
+        Long saveId = lostService.createLost(request.toDto(), nickname, file);
+        log.debug("saveId={}", saveId);
+
+        return ApiResponse.created(saveId);
+    }
 }
--- ireon/src/main/java/com/uhpoo/ireon/api/service/lost/LostService.java
+++ ireon/src/main/java/com/uhpoo/ireon/api/service/lost/LostService.java
@@ -1,4 +1,33 @@
 package com.uhpoo.ireon.api.service.lost;
 
+import com.uhpoo.ireon.api.service.lost.dto.CreateLostDto;
+import com.uhpoo.ireon.domain.lost.repository.LostRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 실종동물 게시글 서비스
+ *
+ * @author yekk1
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class LostService {
+
+    private final LostRepository lostRepository;
+
+    /**
+     * 실종동물 게시글 등록
+     *
+     *  @param dto 실종동물 게시글 정보
+     *  @param nickname 현재 로그인 중인 회원 닉네임
+     *  @param file 첨부파일
+     *  @return 등록된 PK 값
+     */
+    public Long createLost(CreateLostDto dto, String nickname, MultipartFile file) {
+        return null;
+    }
 }
