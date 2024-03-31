92f187af396614b58bdbfd850fb24506f11b2c68
yoojinjangjang <yoojinjangjang@gmail.com>
2024-03-31 20:22:07 +0900

    IREON-76 /[회원APIDocs생성] feat: 회원프로필등록및수정 API 스켈레톤 작성

--- ireon/src/main/java/com/uhpoo/ireon/api/controller/member/MemberController.java
+++ ireon/src/main/java/com/uhpoo/ireon/api/controller/member/MemberController.java
@@ -2,10 +2,7 @@ package com.uhpoo.ireon.api.controller.member;
 
 import com.uhpoo.ireon.api.ApiResponse;
 import com.uhpoo.ireon.api.controller.member.request.*;
-import com.uhpoo.ireon.api.controller.member.response.EmailVerificationResponse;
-import com.uhpoo.ireon.api.controller.member.response.MemberResponse;
-import com.uhpoo.ireon.api.controller.member.response.MemberSignUpResponse;
-import com.uhpoo.ireon.api.controller.member.response.TokenResponse;
+import com.uhpoo.ireon.api.controller.member.response.*;
 import com.uhpoo.ireon.api.service.member.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
@@ -14,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @RestController
@@ -136,6 +134,21 @@ public class MemberController {
         memberService.deleteMember(request);
 
         return ApiResponse.ok(null);
+    }
+
+    @PostMapping("/profile")
+    public ApiResponse<?> updateMemberProfileImage(@RequestParam @Email String email,
+                                                   @RequestPart(value = "image", required = false) MultipartFile image) {
+
+
+        log.debug("MemberController#updateMemberProfileImage called.");
+        log.debug("email={}", email);
+        log.debug("MultipartFile={}",image);
+
+        // TODO: 로그인 인증, 이메일 존재 여부 확인
+        MemberProfileImageResponse response = memberService.updateMemberProfileImage(email, image);
+
+        return ApiResponse.ok(response);
     }
 
 }
--- ireon/src/main/java/com/uhpoo/ireon/api/service/member/MemberService.java
+++ ireon/src/main/java/com/uhpoo/ireon/api/service/member/MemberService.java
@@ -1,10 +1,7 @@
 package com.uhpoo.ireon.api.service.member;
 
 import com.uhpoo.ireon.api.controller.member.request.MemberDeleteRequest;
-import com.uhpoo.ireon.api.controller.member.response.EmailVerificationResponse;
-import com.uhpoo.ireon.api.controller.member.response.MemberResponse;
-import com.uhpoo.ireon.api.controller.member.response.MemberSignUpResponse;
-import com.uhpoo.ireon.api.controller.member.response.TokenResponse;
+import com.uhpoo.ireon.api.controller.member.response.*;
 import com.uhpoo.ireon.api.service.member.dto.MemberLoginDto;
 import com.uhpoo.ireon.api.service.member.dto.MemberSignUpDto;
 import com.uhpoo.ireon.api.service.member.dto.MemberUpdateDto;
@@ -12,6 +9,7 @@ import com.uhpoo.ireon.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @Service
@@ -51,5 +49,9 @@ public class MemberService {
     }
 
     public void deleteMember(MemberDeleteRequest request) {
+    }
+
+    public MemberProfileImageResponse updateMemberProfileImage(String email, MultipartFile image) {
+        return null;
     }
 }
