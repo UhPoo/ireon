7f569592b6517bfe2a95eabbac0a0a8d8e5662da
yekk1 <daeyoi96@gmail.com>
2024-04-15 21:36:29 +0900

    IREON-40/[실종동물전체조회] feat: 실종동물 전체조회 API 스켈레톤 작성

---
+++ ireon/src/main/java/com/uhpoo/ireon/api/service/lost/LostQueryService.java
@@ -0,0 +1,33 @@
+package com.uhpoo.ireon.api.service.lost;
+
+import com.uhpoo.ireon.api.PageResponse;
+import com.uhpoo.ireon.api.controller.lost.response.LostResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * 실종동물 게시글 조회 서비스
+ *
+ * @author yekk1
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class LostQueryService {
+
+    private final LostQueryService lostQueryService;
+
+    /**
+     * 실종동물 목록 조회
+     *
+     * @return 검색 조건에 해당하는 실종동물 목록
+     */
+    public PageResponse<List<LostResponse>> getLosts() {
+        return null;
+    }
+}
