1f731e6cf621772fc72d8ba0c8a826067999d114
longBright <longbright0804@gmail.com>
2024-04-16 14:55:36 +0900

    IREON-138/[유기동물 댓글 등록] feat: 유기동물 게시판 댓글 레포지토리 생성 및 JPA 적용

---
+++ ireon/src/main/java/com/uhpoo/ireon/domain/abandon/repository/AbandonCommentRepository.java
@@ -0,0 +1,14 @@
+package com.uhpoo.ireon.domain.abandon.repository;
+
+import com.uhpoo.ireon.domain.abandon.AbandonComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 유기동물 게시글 댓글 CUD 레포지토리
+ *
+ * @author 최영환
+ */
+@Repository
+public interface AbandonCommentRepository extends JpaRepository<AbandonComment, Long> {
+}
