abeec8c68c13e9c6ce368a66104e74961d46b8d8
yekk1 <daeyoi96@gmail.com>
2024-04-17 00:34:44 +0900

    IREON-147/[실종동물-스크랩] feat: 실종동물 스크랩 엔티티 생성

---
+++ ireon/src/main/java/com/uhpoo/ireon/domain/lost/LostScrap.java
@@ -0,0 +1,44 @@
+package com.uhpoo.ireon.domain.lost;
+
+import com.uhpoo.ireon.domain.common.TimeBaseEntity;
+import com.uhpoo.ireon.domain.member.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 실종동물 스크랩 엔티티
+ *
+ * @author yekki
+ */
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LostScrap extends TimeBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "crap_id")
+    private Long scrapId;
+
+    @Column(nullable = false)
+    private Boolean active;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "lost_id")
+    private Lost lost;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Builder
+    public LostScrap(Long scrapId, Boolean active, Lost lost, Member member) {
+        this.scrapId = scrapId;
+        this.active = active;
+        this.lost = lost;
+        this.member = member;
+    }
+}
