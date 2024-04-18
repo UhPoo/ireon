db877f7b2f5266babfecfc6e2461d577f03c60a4
longBright <longbright0804@gmail.com>
2024-04-18 16:49:31 +0900

    IREON-194/[공지사항 등록] feat: 공지사항 엔티티 생성

--- ireon/src/main/java/com/uhpoo/ireon/domain/notice/Notice.java
+++ ireon/src/main/java/com/uhpoo/ireon/domain/notice/Notice.java
@@ -1,4 +1,45 @@
 package com.uhpoo.ireon.domain.notice;
 
+import com.uhpoo.ireon.domain.member.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 공지사항 게시글 엔티티
+ *
+ * @author 최영환
+ */
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Notice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notice_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "LONGTEXT")
+    private String content;
+
+    @Column(nullable = false)
+    private Boolean active;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Builder
+    private Notice(String title, String content, Boolean active, Member member) {
+        this.title = title;
+        this.content = content;
+        this.active = active;
+        this.member = member;
+    }
 }
