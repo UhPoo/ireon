7016d963a588d1cd38bbc9ec72a35a3cfda4c874
yekk1 <daeyoi96@gmail.com>
2024-05-07 20:55:29 +0900

    IREON-147/[실종동물-스크랩]: 실종동물 스크랩 조회 API 스켈레톤 작성

--- ireon/src/main/java/com/uhpoo/ireon/api/controller/lost/LostScrapController.java
+++ ireon/src/main/java/com/uhpoo/ireon/api/controller/lost/LostScrapController.java
@@ -1,13 +1,18 @@
 package com.uhpoo.ireon.api.controller.lost;
 
 import com.uhpoo.ireon.api.ApiResponse;
+import com.uhpoo.ireon.api.PageResponse;
 import com.uhpoo.ireon.api.controller.lost.request.AddLostScrapRequest;
+import com.uhpoo.ireon.api.controller.lost.response.LostResponse;
+import com.uhpoo.ireon.api.service.lost.LostQueryService;
 import com.uhpoo.ireon.api.service.lost.LostScrapService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 실종동물 스크랩 API 컨트롤러
@@ -21,6 +26,7 @@ import org.springframework.web.bind.annotation.*;
 public class LostScrapController {
 
     private final LostScrapService lostScrapService;
+    private final LostQueryService lostQueryService;
 
     /**
      * 실종동물 스크랩 추가 API
@@ -38,5 +44,23 @@ public class LostScrapController {
         log.debug("addId={}", addId);
 
         return ApiResponse.created(addId);
+    }
+
+    /**
+     * 스크랩한 실종동물 게시글 목록 조회 API
+     *
+     * @return 스크랩한 실종동물 게시글 목록
+     */
+    @GetMapping
+    public ApiResponse<PageResponse<List<LostResponse>>> getLostScraps() {
+        log.debug("LostScrapController#getLostScrap called.");
+
+        String nickname = "nickname";
+        log.debug("nickname={}", nickname);
+
+        PageResponse<List<LostResponse>> response = lostQueryService.getLostScraps(nickname);
+        log.debug("response={}", response);
+
+        return ApiResponse.ok(response);
     }
 }
