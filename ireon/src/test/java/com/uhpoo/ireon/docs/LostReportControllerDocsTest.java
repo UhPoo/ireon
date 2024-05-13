9898c1f5cc5f254f2bc8accb5f966335db5397b3
yekk1 <daeyoi96@gmail.com>
2024-05-14 01:40:23 +0900

    IREON-149/[실종동물 게시글 신고] docs: 실종동물 게시글 신고 신고 조회 API Docs 작성

--- ireon/src/test/java/com/uhpoo/ireon/docs/LostReportControllerDocsTest.java
+++ ireon/src/test/java/com/uhpoo/ireon/docs/LostReportControllerDocsTest.java
@@ -1,6 +1,9 @@
 package com.uhpoo.ireon.docs;
 
+import com.uhpoo.ireon.api.PageResponse;
 import com.uhpoo.ireon.api.controller.lost.LostReportController;
+import com.uhpoo.ireon.api.controller.lost.response.LostReportResponse;
+import com.uhpoo.ireon.api.service.lost.LostReportQueryService;
 import com.uhpoo.ireon.api.service.lost.LostReportService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,13 +12,14 @@ import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.security.test.context.support.WithMockUser;
 
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
@@ -32,10 +36,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(LostReportControllerDocsTest.class)
 public class LostReportControllerDocsTest extends RestDocsSupport{
     private final LostReportService lostReportService= mock(LostReportService.class);
+    private final LostReportQueryService lostReportQueryService = mock(LostReportQueryService.class);
 
     @Override
     protected Object initController() {
-        return new LostReportController(lostReportService);
+        return new LostReportController(lostReportService, lostReportQueryService);
     }
     @DisplayName("실종동물 게시글 신고 등록 API")
     @Test
@@ -99,6 +104,65 @@ public class LostReportControllerDocsTest extends RestDocsSupport{
                                         .description("메시지"),
                                 fieldWithPath("data").type(JsonFieldType.NUMBER)
                                         .description("삭제된 PK 값")
+                        )
+                ));
+    }
+
+    @DisplayName("신고한 실종동물 게시글 전체 조회 API")
+    @Test
+    @WithMockUser
+    void getLostReports() throws Exception {
+        LostReportResponse item1 = LostReportResponse.builder()
+                .lostReportId(1L)
+                .lostId(10L)
+                .content("거짓 신고글")
+                .createdTime("2024-05-01")
+                .build();
+
+        LostReportResponse item2 = LostReportResponse.builder()
+                .lostReportId(2L)
+                .lostId(11L)
+                .content("펫숍 광고글")
+                .createdTime("2024-05-02")
+                .build();
+
+        List<LostReportResponse> items = List.of(item1, item2);
+
+        PageResponse<List<LostReportResponse>> response = PageResponse.of(false, items);
+
+        given(lostReportQueryService.getLostReports(anyString()))
+                .willReturn(response);
+
+        mockMvc.perform(
+                        get("/lost/report")
+                                .header("Authentication", "authentication")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("get-lost-reports",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        responseFields(
+                                fieldWithPath("code").type(JsonFieldType.NUMBER)
+                                        .description("코드"),
+                                fieldWithPath("status").type(JsonFieldType.STRING)
+                                        .description("상태"),
+                                fieldWithPath("message").type(JsonFieldType.STRING)
+                                        .description("메시지"),
+                                fieldWithPath("data").type(JsonFieldType.OBJECT)
+                                        .description("응답 데이터"),
+                                fieldWithPath("data.hasNext").type(JsonFieldType.BOOLEAN)
+                                        .description("다음 페이지 존재 여부"),
+                                fieldWithPath("data.items").type(JsonFieldType.ARRAY)
+                                        .description("게시글 목록"),
+                                fieldWithPath("data.items[].lostReportId").type(JsonFieldType.NUMBER)
+                                        .description("실종동물 게시글 신고 PK"),
+                                fieldWithPath("data.items[].lostId").type(JsonFieldType.NUMBER)
+                                        .description("실종동물 게시글 PK"),
+                                fieldWithPath("data.items[].content").type(JsonFieldType.STRING)
+                                        .description("신고 글 내용"),
+                                fieldWithPath("data.items[].createdTime").type(JsonFieldType.STRING)
+                                        .description("작성일")
                         )
                 ));
     }
